1310e7c4b1750d560543b3aa8ae640829ba26f5a
Facundo Minicucci <facundo.minicucci@wirsolut.com>
2024-01-13 15:51:56 -0300

    Cambios en el css y realización de la cuenta regresiva

--- clase6/js/cuenta-regresiva.js
+++ clase6/js/cuenta-regresiva.js
@@ -5,6 +5,7 @@ const txtMinutos = document.querySelector('#txtMinutos');
 const txtSegundos = document.querySelector('#txtSegundos');
 
 const h1 = document.querySelector('h1');
+let intervalo = '';
 
 //declaramos funcion/es de control
 function cuentaRegresiva()
@@ -16,7 +17,44 @@ function cuentaRegresiva()
         //expresada en milisegundos
     let diferencia = final - actual;
     console.log('diferencia:', diferencia);
+
+    /* convertimos a unidades de tiempo */
+    //obtenemos el tiempo expresada en segundos (sin decimales)
+    let segundos = Math.trunc(diferencia / 1000); //trunc sirve para sacar los decimales
+    console.log( 'segundos: ', segundos);
+
+    //obtenemos el tiempo expresada en minutos (sin decimales)
+    let minutos = Math.trunc( segundos / 60);
+    console.log( 'minutos: ', minutos);
+
+    //obtenemos el tiempo expresada en horas (sin decimales)
+    let horas = Math.trunc( minutos / 60);
+    console.log( 'horas: ', horas);
+
+    //obtenemos el tiempo expresada en dias (sin decimales)
+    let dias = Math.trunc( horas / 24);
+    console.log( 'dias: ', dias);
+
+    /* calculamos los tiempos restantes*/ 
+    horas = horas%24;
+    console.log('horas: ', horas);
+    
+    minutos = minutos%60;
+    console.log( 'minutos: ', minutos);
+
+    segundos = segundos%60;
+    console.log( 'segundos: ', segundos);
+
+
+    /*imprimimos en los span */
+        txtDias.innerText = dias;
+        txtHoras.innerText = horas;
+        txtMinutos.innerText = minutos;
+        txtSegundos.innerText = segundos;
+        
 }
 
 //invocamos a la funcion 
 cuentaRegresiva();
+//actualizamos el llamado a la funcion
+intervalo = setInterval( cuentaRegresiva, 1000 );

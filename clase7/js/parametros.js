ab07af512077e5500aba1d5860b93b033a9670c1
MalenAzarioGalli <malenazario_2016@hotmail.com>
2024-01-13 23:41:21 -0300

    Clase número 7: creación de eventos y parámetros, con botones funcionales (cambian de color) y utilización de addEventListener.

--- clase7/js/parametros.js
+++ clase7/js/parametros.js
@@ -1,5 +1,9 @@
 //ubicamos dentro elementos dentro del DOM
 const caja = document.querySelector('#caja');
+    //los a que estan dentro de #navegacion 
+const botones = document.querySelectorAll('#navegacion a');
+
+console.log( botones );
 
 /*declaracion de funciones */
 function saludar( nombre )
@@ -25,3 +29,23 @@ function pintarNaranja()
 saludar('Luna');
 saludar('Facundo');
 saludar('Milo');
+
+//centralizamos el código en el JavaScript
+botones[0].addEventListener(
+    'click',
+    ()=>{
+        pintar('hsl(120, 50%, 50%)');
+    }
+);
+botones[1].addEventListener(
+    'click',
+    ()=>{
+        pintar('hsl(30, 50%, 50%)');
+    }
+);
+botones[2].addEventListener(
+    'click',
+    ()=>{
+        pintar('hsl(200, 80%, 70%)')
+    }
+);
